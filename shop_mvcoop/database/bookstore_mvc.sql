ad7cc8573bc523dbcb2abe11940f249d4e954603
Thai Hop <56145900+funnyHop@users.noreply.github.com>
2022-09-25 00:34:58 +0700

    Update bookstore_mvc.sql

--- shop_mvcoop/database/bookstore_mvc.sql
+++ shop_mvcoop/database/bookstore_mvc.sql
@@ -2,10 +2,10 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th9 05, 2022 lúc 09:40 AM
--- Phiên bản máy phục vụ: 10.4.24-MariaDB
--- Phiên bản PHP: 8.0.19
+-- Host: 127.0.0.1
+-- Generation Time: Sep 24, 2022 at 07:31 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,83 +18,152 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Cơ sở dữ liệu: `bookstore_mvc`
+-- Database: `database_storebook`
 --
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `tbl_admin`
+-- Table structure for table `acounts`
 --
 
-CREATE TABLE `tbl_admin` (
-  `adminId` int(11) NOT NULL,
-  `adminName` varchar(255) NOT NULL,
-  `adminEmail` varchar(150) NOT NULL,
-  `adminUser` varchar(255) NOT NULL,
-  `adminPass` varchar(255) NOT NULL,
-  `level` int(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Đang đổ dữ liệu cho bảng `tbl_admin`
---
-
-INSERT INTO `tbl_admin` (`adminId`, `adminName`, `adminEmail`, `adminUser`, `adminPass`, `level`) VALUES
-(1, 'Toanvippro', 'ktoan515@gmail.com', 'Toanadmin', 'e10adc3949ba59abbe56e057f20f883e', 0);
+CREATE TABLE `acounts` (
+  `ac_id` int(11) NOT NULL,
+  `cart_id` int(11) NOT NULL,
+  `use_name` varchar(255) COLLATE utf8_vietnamese_ci NOT NULL,
+  `pass` varchar(255) COLLATE utf8_vietnamese_ci NOT NULL,
+  `add` varchar(255) COLLATE utf8_vietnamese_ci NOT NULL,
+  `sex` tinyint(1) NOT NULL,
+  `phone` varchar(255) COLLATE utf8_vietnamese_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_vietnamese_ci NOT NULL,
+  `role` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `tbl_category`
+-- Table structure for table `bill`
 --
 
-CREATE TABLE `tbl_category` (
-  `catId` int(11) NOT NULL,
-  `catName` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `bill` (
+  `bill_id` int(11) NOT NULL,
+  `bill_amount` int(11) NOT NULL,
+  `total` double NOT NULL,
+  `created_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
+
+-- --------------------------------------------------------
 
 --
--- Đang đổ dữ liệu cho bảng `tbl_category`
+-- Table structure for table `bill_detail`
 --
 
-INSERT INTO `tbl_category` (`catId`, `catName`) VALUES
-(1, 'Tâm lý &amp; Kỹ năng sống'),
-(2, 'Công nghệ thông tin'),
-(3, 'Văn học Việt Nam'),
-(4, 'Truyện tranh');
+CREATE TABLE `bill_detail` (
+  `bill_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `note` varchar(255) COLLATE utf8_vietnamese_ci NOT NULL,
+  `bd_amount` int(11) NOT NULL,
+  `bd_price` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
+
+-- --------------------------------------------------------
 
 --
--- Chỉ mục cho các bảng đã đổ
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `cart_id` int(11) NOT NULL,
+  `ac_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart_detail`
+--
+
+CREATE TABLE `cart_detail` (
+  `cart_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `cd_amount` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `catetory`
+--
+
+CREATE TABLE `catetory` (
+  `cate_id` int(11) NOT NULL,
+  `cate_name` varchar(255) COLLATE utf8_vietnamese_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `product_id` int(11) NOT NULL,
+  `ac_id` int(11) NOT NULL,
+  `cate_id` int(11) NOT NULL,
+  `product_name` varchar(255) COLLATE utf8_vietnamese_ci NOT NULL,
+  `describe` text COLLATE utf8_vietnamese_ci NOT NULL,
+  `product_price` double NOT NULL,
+  `category` varchar(255) COLLATE utf8_vietnamese_ci NOT NULL,
+  `author` varchar(255) COLLATE utf8_vietnamese_ci NOT NULL,
+  `image` varchar(255) COLLATE utf8_vietnamese_ci NOT NULL,
+  `product_created_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
+
+--
+-- Indexes for dumped tables
 --
 
 --
--- Chỉ mục cho bảng `tbl_admin`
+-- Indexes for table `acounts`
 --
-ALTER TABLE `tbl_admin`
-  ADD PRIMARY KEY (`adminId`);
+ALTER TABLE `acounts`
+  ADD PRIMARY KEY (`ac_id`);
 
 --
--- Chỉ mục cho bảng `tbl_category`
+-- Indexes for table `bill`
 --
-ALTER TABLE `tbl_category`
-  ADD PRIMARY KEY (`catId`);
+ALTER TABLE `bill`
+  ADD PRIMARY KEY (`bill_id`);
 
 --
--- AUTO_INCREMENT cho các bảng đã đổ
+-- Indexes for table `bill_detail`
 --
+ALTER TABLE `bill_detail`
+  ADD PRIMARY KEY (`bill_id`,`product_id`);
 
 --
--- AUTO_INCREMENT cho bảng `tbl_admin`
+-- Indexes for table `cart`
 --
-ALTER TABLE `tbl_admin`
-  MODIFY `adminId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`cart_id`);
 
 --
--- AUTO_INCREMENT cho bảng `tbl_category`
+-- Indexes for table `cart_detail`
 --
-ALTER TABLE `tbl_category`
-  MODIFY `catId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE `cart_detail`
+  ADD PRIMARY KEY (`cart_id`,`product_id`);
+
+--
+-- Indexes for table `catetory`
+--
+ALTER TABLE `catetory`
+  ADD PRIMARY KEY (`cate_id`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`product_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
